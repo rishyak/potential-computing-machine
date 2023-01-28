cad8ec1085f88b48573130ad5cd24ac9755a8fe6
rishyak <hello@rishyak.com>
2023-01-28 01:04:26 -0500

    ex3
    Signed-off-by: rishyak <hello@rishyak.com>

--- Labs/2/rtl/Exercise3.sv
+++ Labs/2/rtl/Exercise3.sv
@@ -7,4 +7,11 @@ module Exercise3 (
     output [15:0] out
 );
 
+    wire [7:0] d_a_in, d_b_in;
+
+    Mystery1 Alpha(.a(a[1:0]), .b(b[7:0]), .c(c[7:0]), .d(d_a_in));
+    Mystery1 Beta(.a(a[3:2]), .b(b[15:8]), .c(c[15:8]), .d(d_b_in));
+
+    Mystery2 Gamma(.clk(clk), .nReset(nReset), .a_in(d_a_in), .b_in(d_b_in), .out(out));
+
 endmodule
